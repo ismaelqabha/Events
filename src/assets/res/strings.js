1b27d4b364484a857e5ad417ea5957ad6d2e78f5
fadishurrush@gmail.com <114020115+fadishurrush@users.noreply.github.com>
2023-09-29 14:15:27 +0300

    Preperations
    transfered all the texts in Both the
    1.ProviderChooseService
    2.ProviderCreateListing
    Screens into the
    Strings File
    
    cleaned and sorted the functions out
    
    new Functions :
    1.showMessage
    2.confirmationBox
    
    1. shows a message depending on the situation
    
    2. a confirmation pop up to make sure the user wants to leave

--- src/assets/res/strings.js
+++ src/assets/res/strings.js
@@ -1,5 +1,22 @@
 const strings = {
-    cardtxt1:'على استعداد لتلبية جميع مناسباتكم' ,
-}
+  cardtxt1: 'على استعداد لتلبية جميع مناسباتكم',
+  arabic: {
+    ProviderCreateListing: {
+      HeadText: 'أهلا وسهلا, اسماعيل',
+      StartService: 'ابدأ بخدمة جديده',
+      NewService: 'انشاء خدمة جديده',
+    },
+    ProviderChooseService:{
+        HeadText:"ما هو تصنيف الخدمة التي توفرها للزبائن؟",
+        back:"رجوع",
+        next:"التالي",
+        showMessage:"الرجاء اختيار التصنيف قبل الاستمرار",
+        warning: "انتباه" ,
+        backWarning:"الخروج سوف يحذف كل شيء",
+        cancelButton:"البقاء",
+        confirmButton:"الخروج",
+    }
+  },
+};
 
-export default strings ;
+export default strings;
--- src/screens/ProviderScreens/ProviderCreateListing.js
+++ src/screens/ProviderScreens/ProviderCreateListing.js
@@ -1,140 +1,175 @@
-import React, { useState, useContext } from 'react';
-import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import { TouchableOpacity } from 'react-native';
+import React, {useState, useContext} from 'react';
+import {View, StyleSheet, Text, TouchableHighlight} from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import {TouchableOpacity} from 'react-native';
 import SearchContext from '../../../store/SearchContext';
 import PoviderServiceListCard from '../../components/PoviderServiceListCard';
-import { ScreenNames } from '../../../route/ScreenNames';
-import 'react-native-get-random-values'
-import { v4 as uuidv4 } from 'uuid';
+import {ScreenNames} from '../../../route/ScreenNames';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
+import strings from '../../assets/res/strings';
 
+const ProviderCreateListing = props => {
+  const [supmeted, setSupmeted] = useState(false);
+  const {userId, ServiceDataInfo, setServiceDataInfo, setServId, ServId} =
+    useContext(SearchContext);
 
-const ProviderCreateListing = (props) => {
-    const [supmeted, setSupmeted] = useState(false)
-    const { userId, ServiceDataInfo, setServiceDataInfo, setServId, ServId } = useContext(SearchContext);
+    const language = strings.arabic.ProviderCreateListing
 
-    let SId = uuidv4();
+  let SId = uuidv4();
 
-    const onPressHandler = () => {
-        setSupmeted(!supmeted);
-    }
-    const chickIfChecked = () => {
-        const isChecked = ServiceDataInfo.find(item => item.service_id === SId)
-        return !!isChecked;
-    }
+  const onPressHandler = () => {
+    setSupmeted(!supmeted);
+  };
+  const chickIfChecked = () => {
+    const isChecked = ServiceDataInfo.find(item => item.service_id === SId);
+    return !isChecked;
+  };
 
-    const onStartPress = () => {
-        setServId(SId)
-        console.log("chickIfChecked", chickIfChecked());
-        const AddNewService = {
-            service_id: SId,
-            UserId: userId,
-            workingRegion: [],
-        }
-
-        let ServiceArr = ServiceDataInfo;
-        if (!chickIfChecked()) {
-            ServiceArr.push(AddNewService);
-            setServiceDataInfo([...ServiceArr])
-        } else {
-            ServiceArr = ServiceArr.filter(ser => ser.service_id != SId)
-            setServiceDataInfo([...ServiceArr])
-        }
-        
-        console.log(ServiceDataInfo);
-        props.navigation.navigate(ScreenNames.ProviderChooseService, { data: { ...props }, isFromChooseServiceClick: true });
-    }
-
-    const query = () => {
-        return ServiceDataInfo.filter(id => {
-            return id.UserId == userId;
-        })
-    }
-    const renderService = () => {
-        const data = query();
-        const cardsArray = data.map(card => {
-            return <PoviderServiceListCard  {...card} />;
-        });
-        return cardsArray;
+  const onStartPress = () => {
+    setServId(SId);
+    console.log('chickIfChecked', chickIfChecked());
+    const AddNewService = {
+      service_id: SId,
+      UserId: userId,
+      workingRegion: [],
     };
 
+    let ServiceArr = ServiceDataInfo;
+    if (!chickIfChecked()) {
+      ServiceArr.push(AddNewService);
+      setServiceDataInfo([...ServiceArr]);
+    } else {
+      ServiceArr = ServiceArr.filter(ser => ser.service_id != SId);
+      setServiceDataInfo([...ServiceArr]);
+    }
+
+    props.navigation.navigate(ScreenNames.ProviderChooseService, {
+      data: {...props},
+      isFromChooseServiceClick: true,
+    });
+  };
+
+  const query = () => {
+    return ServiceDataInfo.filter(id => {
+      return id.UserId == userId;
+    });
+  };
+  const renderService = () => {
+    const data = query();
+    const cardsArray = data.map(card => {
+      return <PoviderServiceListCard {...card} />;
+    });
+    return cardsArray;
+  };
+
+  //   the head text
+  const HeadText = () => {
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.headText}>أهلا وسهلا, اسماعيل</Text>
-            </View>
-            <View style={styles.body}>
-                {renderService()}
-            </View>
-            <Text style={styles.line}>____________________________________________________</Text>
-            <View style={styles.footer}>
-                <Text style={styles.footerText}>ابدأ بخدمة جديده</Text>
-                <TouchableOpacity style={styles.AddButton} onPress={onStartPress}
-                //activeOpacity={0.2} underlayColor={supmeted ? 'white' : 'gray'}
-                >
-                    <AntDesign
-                        name='plussquareo'
-                        style={{ fontSize: 30, alignSelf: 'center', marginRight: 30 }}
-                    />
-                    <Text style={styles.footText}>انشاء خدمة جديده</Text>
-                    <FontAwesome5
-                        name='less-than'
-                        style={{ fontSize: 20, alignSelf: 'center', marginLeft: 30 }}
-                    />
-                </TouchableOpacity>
-            </View >
-        </View >
+      <View style={styles.header}>
+        <Text style={styles.headText}>
+          {language.HeadText}
+        </Text>
+      </View>
     );
-}
+  };
+
+  //   seperator line
+  const seperator = () => {
+    return (
+      <Text style={styles.line}>
+        ____________________________________________________
+      </Text>
+    );
+  };
+
+  //   footer text component
+  const footerText = () => {
+    return (
+      <Text style={styles.footerText}>
+        {language.StartService}
+      </Text>
+    );
+  };
+
+  //   create a new service component
+  const newService = () => {
+    return (
+      <TouchableOpacity
+        style={styles.AddButton}
+        onPress={() => onStartPress()}
+        //activeOpacity={0.2} underlayColor={supmeted ? 'white' : 'gray'}
+      >
+        <AntDesign name="plussquareo" style={styles.plusSquare} />
+        <Text style={styles.footText}>
+          {language.NewService}
+        </Text>
+        <FontAwesome5 name="less-than" style={styles.lessThan} />
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {HeadText()}
+      <View style={styles.body}>{renderService()}</View>
+      {seperator()}
+      <View style={styles.footer}>
+        {footerText()}
+        {newService()}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    header: {
-        alignItems: 'flex-end',
-        marginRight: 30,
-        marginTop: 40,
-        marginBottom: 10,
-    },
-    body: {
-        height: '40%',
-        marginTop: 20,
-    },
-    footer: {
-        //alignItems: 'flex-end',
-        marginTop: 20,
-
-    },
-    AddButton: {
-        flexDirection: 'row-reverse',
-        width: '100%',
-        height: 60,
-        justifyContent: 'space-between'
-    },
-    headText: {
-        fontSize: 25,
-        color: 'black',
-        fontFamily: 'Cairo-VariableFont_slnt,wght',
-    },
-    footerText: {
-        fontSize: 18,
-        color: 'black',
-        marginRight: 30,
-        marginBottom: 10,
-    },
-    footText: {
-        fontSize: 18,
-        color: 'black',
-        alignSelf: 'center',
-        marginLeft: 130,
-    },
-    line: {
-        textAlign: 'center',
-        color: '#d3d3d3'
-    },
-
-})
+  container: {
+    flex: 1,
+  },
+  header: {
+    alignItems: 'flex-end',
+    marginRight: 30,
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  body: {
+    height: '40%',
+    marginTop: 20,
+  },
+  footer: {
+    //alignItems: 'flex-end',
+    marginTop: 20,
+  },
+  AddButton: {
+    flexDirection: 'row-reverse',
+    width: '100%',
+    height: 60,
+    justifyContent: 'space-between',
+  },
+  headText: {
+    fontSize: 25,
+    color: 'black',
+    fontFamily: 'Cairo-VariableFont_slnt,wght',
+  },
+  footerText: {
+    fontSize: 18,
+    color: 'black',
+    marginRight: 30,
+    marginBottom: 10,
+  },
+  footText: {
+    fontSize: 18,
+    color: 'black',
+    alignSelf: 'center',
+    marginLeft: 130,
+  },
+  line: {
+    textAlign: 'center',
+    color: '#d3d3d3',
+  },
+  lessThan: {fontSize: 20, alignSelf: 'center', marginLeft: 30},
+  plusSquare: {fontSize: 30, alignSelf: 'center', marginRight: 30},
+});
 
 export default ProviderCreateListing;
