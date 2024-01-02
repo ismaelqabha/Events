e0226041212b8595401b8eb59b02dc1844364992
ismaelqabha <ismaelqabha.iq@gmail.com>
2024-01-02 22:51:11 +0200

    update calender and waiting list

--- src/screens/ProviderScreens/ProviderWaitingPay.js
+++ src/screens/ProviderScreens/ProviderWaitingPay.js
@@ -1,14 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
 import React from 'react'
+import { colors } from '../../assets/AppColors'
 
 const ProviderWaitingPay = () => {
+
+  const renderClientInfo = () => {
+    return (
+      <View style={styles.info}>
+        <Image style={styles.profilImg} source={require('../../assets/photos/user.png')} />
+        <Text style={styles.userName}>اسماعيل كبها</Text>
+      </View>
+    )
+  }
+  const renderRequestInfo = () => {
+    return (
+      <View style={styles.reqInfo}>
+        <Text style={styles.infoTxt}>تاريخ الطلب : 5/1/2024</Text>
+        <Text style={styles.infoTxt}>تاريخ الحجز : 10/8/2024</Text>
+        <Text style={styles.infoTxt}> السعر : 10000</Text>
+        <View style={styles.buttonView}>
+          <Pressable><Text>اٍلغاء الطلب</Text></Pressable>
+          <Pressable><Text>التفاصيل</Text></Pressable>
+        </View>
+      </View>
+    )
+  }
+
+  const renderClientCard = () => {
+    return (
+      <View style={styles.card}>
+        {renderRequestInfo()}
+        {renderClientInfo()}
+      </View>
+    )
+  }
+
   return (
     <View>
-      <Text>ProviderWaitingPay</Text>
+      {renderClientCard()}
+      {renderClientCard()}
     </View>
   )
 }
 
 export default ProviderWaitingPay
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: 'row',
+    width: '90%',
+    height: 150,
+    alignSelf: 'center',
+    margin: 10
+  },
+  info: {
+    width: '40%',
+    height: '100%',
+    borderRadius: 10,
+    backgroundColor: colors.darkGold,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  reqInfo: {
+    width: '60%',
+    height: '90%',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    elevation: 5,
+    //alignItems: 'center',
+  },
+  profilImg: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+    backgroundColor: colors.BGScereen,
+    marginBottom: 10
+  },
+  userName: {
+    fontSize: 15,
+    color: 'white'
+  },
+  buttonView: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 30,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0
+  },
+  infoTxt:{
+    marginVertical: 5,
+    marginRight: 10,
+    color: colors.puprble,
+    fontSize: 15
+  }
+})
